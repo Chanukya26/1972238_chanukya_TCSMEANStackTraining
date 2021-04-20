5296763fdfc92789820db5c4c79a443c58058cfc
Chanukya <cheekati.4@wright.edu>
2021-04-20 00:35:01 -0400

    add

---
+++ Node JS with MongoDB using mongdb module/JsonInsert.js
@@ -0,0 +1,12 @@
+let mongoClient = require("mongodb").MongoClient;
+let url = "mongodb://localhost:27017";
+mongoClient.connect(url,{ useUnifiedTopology: true },(err1,client)=> {
+    if(!err1){
+        
+        
+
+
+
+
+    }
+})
